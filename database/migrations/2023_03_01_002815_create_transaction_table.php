6f0d3c29781ae75efd76fc4f8d1af046a9db6393
amrkhaled99 <amrkh5255@gmail.com>
2023-03-01 03:14:58 +0200

    DB

---
+++ database/migrations/2023_03_01_002815_create_transaction_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->double("amount")->nullable()->change();      
+            $table->text('description');
+
+            $table->unsignedBigInteger('sellerID');
+            $table->foreign('sellerID')->references('userID')->on('users');
+            $table->unsignedBigInteger('buyerID');
+            $table->foreign('buyerID')->references('userID')->on('users');
+            $table->unsignedBigInteger('assetID');
+            $table->foreign('assetID')->references('assetID')->on('user_Assets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
