607cd0fb96dcb8516a2c04ac82051e8f458453b7
agamy5280 <marawan.agamy2010@hotmail.com>
2023-03-11 22:43:35 +0200

    Added payBill function

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Bill;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Response;
 class BillController extends Controller
 {
     /**
@@ -38,12 +40,35 @@ class BillController extends Controller
             $bill->type=$request->type;
             $bill->amount = $request->amount;
             $bill->description = $request->description;
-            $bill->status = $request->status;
             $bill->due_time =$request->due_time;
             $bill->userID =$request->userID;
             $bill->save();
             return response()->json(['message' => 'success'],201);
         }catch (QueryException $e) {
+            return response()->json(['message' => 'An error occurred while processing your request.'], 500);
+        }
+    }
+    function payBill(Request $request) {
+        try{
+            if(Auth::user()) {
+                $billID = $request->billID;
+                $bill = Bill::where('id', $billID)->first();
+                $userID = Auth::user()->id;
+                $user = User::where('id', $userID)->first();
+                $userBalance = $user->balance;
+                if($userBalance >= $bill->amount) {
+                    $user->balance -= $bill->amount;
+                    $user->save();
+                    $bill->status = "Paid";
+                    $bill->save();
+                    return response()->json(['message' => 'Bill Paid!'],201);
+                } else {
+                    return Response::json("User Balance is not sufficient", 400);
+                }
+            } else {
+                return response()->json(['UnAuthorized'], 401);
+            }
+        } catch(QueryException $e) {
             return response()->json(['message' => 'An error occurred while processing your request.'], 500);
         }
     }
